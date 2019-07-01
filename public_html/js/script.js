198bdc3bc9bd54c96706a98fb0b8456b6705ceca
Julio Freitas <julio.clf31@gmail.com.br>
2019-07-01 16:42:00 -0300

    Atualização email

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -3,11 +3,16 @@ $(function(){
         $('.nav').slideToggle();
     });
     
+    $("#form").submit(function(e) {
+        e.preventDefault();
+      
+alert();
+      });
 });
 
-var data = new Date();
-var adate = data.getDate()+'/'+(data.getMonth()+1)+'/'+data.getFullYear()+' - '+data.getHours()+':'+data.getMinutes();
-var dataEnd = data.toLocaleDateString('pt-br');
+let data = new Date();
+let adate = data.getDate()+'/'+(data.getMonth()+1)+'/'+data.getFullYear()+' - '+data.getHours()+':'+data.getMinutes();
+let dataEnd = data.toLocaleDateString('pt-br');
 let horaEnd = data.toLocaleTimeString();
 
 let dataEl = document.querySelector("#data");
@@ -15,10 +20,11 @@ dataEl.value = dataEnd+' - '+horaEnd;
 
 
 
-var ipEl = document.querySelector('#ip');
+let ipEl = document.querySelector('#ip');
 $.getJSON('https://ipapi.co/json/', function(data) {
     console.log(JSON.stringify(data, null, 2));
     var ip = JSON.stringify(data.ip);
     ipEl.value = JSON.parse(ip);
     console.log(typeof(JSON.parse(ip)))
 });
+
