de29f339cefa0b5c2270e7fbbd1efd71d843557c
Julio Freitas <julio.clf31@gmail.com.br>
2019-07-01 16:09:15 -0300

    Subindo versão para testar envio de formulário

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -4,3 +4,21 @@ $(function(){
     });
     
 });
+
+var data = new Date();
+var adate = data.getDate()+'/'+(data.getMonth()+1)+'/'+data.getFullYear()+' - '+data.getHours()+':'+data.getMinutes();
+var dataEnd = data.toLocaleDateString('pt-br');
+let horaEnd = data.toLocaleTimeString();
+
+let dataEl = document.querySelector("#data");
+dataEl.value = dataEnd+' - '+horaEnd;
+
+
+
+var ipEl = document.querySelector('#ip');
+$.getJSON('https://ipapi.co/json/', function(data) {
+    console.log(JSON.stringify(data, null, 2));
+    var ip = JSON.stringify(data.ip);
+    ipEl.value = JSON.parse(ip);
+    console.log(typeof(JSON.parse(ip)))
+});
