0daf73d054622c652c296e92c17d2ec845aff30c
Julio Freitas <julio.clf31@gmail.com.br>
2019-07-02 13:47:50 -0300

    Validação formulário

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -3,9 +3,25 @@ $(function(){
         $('.nav').slideToggle();
     });
 
+    let data = new Date();
+    //let adate = data.getDate()+'/'+(data.getMonth()+1)+'/'+data.getFullYear()+' - '+data.getHours()+':'+data.getMinutes();
+    let dataEnd = data.toLocaleDateString('pt-br');
+    let horaEnd = data.toLocaleTimeString('pt-br');
+
+    let dataEl = document.querySelector("#data");
+    dataEl.value = dataEnd+' - '+horaEnd;
+
+
+
+    let ipEl = document.querySelector('#ip');
+    $.getJSON('https://ipapi.co/json/', function(data) {
+        console.log(JSON.stringify(data, null, 2));
+        var ip = JSON.stringify(data.ip);
+        ipEl.value = JSON.parse(ip);
+    });
+
     $("#form").submit(function(e) {
         e.preventDefault();
-      
         var $form = $(this);
         $.post($form.attr("action"), $form.serialize()).then(function() {
           alert("Thank you!");
@@ -13,21 +29,6 @@ $(function(){
       });
 });
 
-let data = new Date();
-//let adate = data.getDate()+'/'+(data.getMonth()+1)+'/'+data.getFullYear()+' - '+data.getHours()+':'+data.getMinutes();
-let dataEnd = data.toLocaleDateString('pt-br');
-let horaEnd = data.toLocaleTimeString('pt-br');
 
-let dataEl = document.querySelector("#data");
-dataEl.value = dataEnd+' - '+horaEnd;
-
-
-
-let ipEl = document.querySelector('#ip');
-$.getJSON('https://ipapi.co/json/', function(data) {
-    console.log(JSON.stringify(data, null, 2));
-    var ip = JSON.stringify(data.ip);
-    ipEl.value = JSON.parse(ip);
-});
 
 
